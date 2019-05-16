ab2959d34b5a76d0a4a87b9e997cf1c12a265f42
Jan <goarano@gmail.com>
2019-05-16 14:33:40 +0200

    implemented market_summaries()

--- src/cryptowatch/rest.rs
+++ src/cryptowatch/rest.rs
@@ -11,19 +11,19 @@ fn crypto_request<T: IntoUrl>(url: T) -> Result<CryptowatchResponse, Error> {
     Ok(res_json)
 }
 
-pub fn market_summaries() -> Result<Value, Error> {
+pub fn market_summaries() -> Result<HashMap<String, HashMap<String, MarketSummary>>, Error> {
     let url_str = "https://api.cryptowat.ch/markets/summaries";
     let response = crypto_request(url_str)?;
     let mut response_map: Map<String, Value> = response.result.as_object().unwrap().to_owned();
 
     let keys_strings: Vec<String> = response_map.keys().cloned().collect::<Vec<_>>();
-    let keys: Vec<&str> = keys_strings.iter().map(AsRef::as_ref).collect();
+    let keys: Vec<String> = keys_strings.iter().map(ToOwned::to_owned).collect();
 
-    let market_pairs: Vec<(&str, &str, MarketSummary)> = keys
+    let market_pairs: Vec<(String, String, MarketSummary)> = keys
         .into_iter()
         .filter_map(|k| {
             response_map
-                .remove(k)
+                .remove(&k)
                 .and_then(|v| serde_json::from_value(v).ok())
                 .map(|v: MarketSummary| (k, v))
         })
@@ -31,8 +31,8 @@ pub fn market_summaries() -> Result<Value, Error> {
             let mut market_pair: Vec<&str> = k.split(":").collect();
             match market_pair.len() {
                 2 => {
-                    let market = market_pair.remove(0).clone();
-                    let pair = market_pair.remove(0).clone();
+                    let market = market_pair.remove(0).to_owned();
+                    let pair = market_pair.remove(0).to_owned();
                     Some((market, pair, v))
                 }
                 _ => {
@@ -42,21 +42,21 @@ pub fn market_summaries() -> Result<Value, Error> {
         })
         .collect();
 
-    let markets: Vec<&str> = market_pairs
+    let markets: Vec<_> = market_pairs
         .iter()
-        .map(|(market, _, _)| *market)
+        .map(|(market, _, _)| market)
         .unique()
         .collect();
     println!("{:?}", markets);
 
-    let pairs: Vec<&str> = market_pairs
+    let pairs: Vec<_> = market_pairs
         .iter()
-        .map(|(_, pair, _)| *pair)
+        .map(|(_, pair, _)| pair)
         .unique()
         .collect();
     println!("{:?}", pairs);
 
-    let mut market_pair_map: HashMap<&str, HashMap<&str, MarketSummary>> = HashMap::new();
+    let mut market_pair_map: HashMap<String, HashMap<String, MarketSummary>> = HashMap::new();
     market_pairs
         .into_iter()
         .for_each(|(market, pair, summary)| {
@@ -66,5 +66,5 @@ pub fn market_summaries() -> Result<Value, Error> {
                 .insert(pair, summary);
         });
 
-    Ok(response.result)
+    Ok(market_pair_map)
 }
