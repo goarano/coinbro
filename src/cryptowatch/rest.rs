6ca55bf9a08b337652479bf29a4dbe2c0e39bf8b
Jan <goarano@gmail.com>
2019-05-22 16:47:11 +0200

    collecting tokio results

--- src/cryptowatch/rest.rs
+++ src/cryptowatch/rest.rs
@@ -1,8 +1,9 @@
 use crate::cryptowatch::data::*;
 use futures::{stream, Future, Stream};
 //use hyper::{Client, Uri};
-use reqwest::r#async::Client;
+use reqwest::r#async::{Chunk, Client, Response};
 use reqwest::{Error, IntoUrl};
+use std::sync::{Arc, Mutex};
 use tokio;
 
 pub fn cryptowatch_get<T>(url: T) -> Result<CryptowatchResponse, Error>
@@ -38,14 +39,31 @@ pub fn cryptowatch_get_future() {
             client
                 .get(url)
                 .send()
-                .and_then(|r| r.into_body().concat2().from_err())
+                .and_then(|r: Response| r.into_body().concat2().from_err())
         })
         .buffer_unordered(4);
+    let res_arc = Arc::new(Mutex::new(Vec::<Result<String, String>>::new()));
     let work = bodies
-        .for_each(|p| {
-            println!("work done: {:?}", &p);
-            Ok(())
+        .for_each({
+            let res = Arc::clone(&res_arc);
+            move |p: Chunk| {
+                println!("work done: {:?}", &p);
+                let v = p.to_vec();
+                let b = String::from_utf8_lossy(&v).to_string();
+                res.lock().unwrap().push(Ok(b));
+                Ok(())
+            }
         })
-        .map_err(|e| println!("work not done: {}", e));
+        .map_err({
+            let res = Arc::clone(&res_arc);
+            move |e| {
+                println!("work not done: {}", e);
+                res.lock().unwrap().push(Err(e.to_string()));
+            }
+        });
+
     tokio::run(work);
+
+    let res = res_arc.lock().unwrap();
+    println!("{:?}", &res_arc);
 }
